8393857e2b628b1ab011b96c7232c6df824e221a
GwenDumoulin <gwen.dumoulin@gmail.com>
2024-09-30 16:01:28 +0200

    Add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,24 @@
+import Banner from '../../components/Banner'
+import banniere2 from '../../../public/image_source2.png'
+import Accordion from '../../components/Accordion'
+import data from '../../data/about.json'
+
+function About() {
+  return (
+    <main className="mx-[100px] my-0 min-h-[953px]">
+      <Banner banniere={banniere2} text="A propos de Kasa" />
+
+      <div className="flex jestify-center flex-col items-center gap-[30px] py-[50px]">
+        {data.map((element, index) => (
+          <Accordion
+            key={index}
+            title={element.title}
+            content={element.description}
+          />
+        ))}
+      </div>
+    </main>
+  )
+}
+
+export default About
